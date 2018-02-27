74c04c62d2ffb3d377b41619f215c13d12c71da4
Li-Ming-Fan <li_m_f@163.com>
2018-02-27 22:39:18 +0800

    Add files via upload

---
+++ model_recog_data.py
@@ -0,0 +1,116 @@
+ # -*- coding: utf-8 -*-
+
+import os
+
+from PIL import Image
+import numpy as np
+
+import model_comm_meta as meta
+
+
+'''
+#
+dir_data = './data_generated'
+dir_images = dir_data + '/images'
+dir_contents = dir_data + '/contents'
+#
+'''
+
+
+#
+def getFilesInDirect(path, str_dot_ext):
+    file_list = []
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)  
+        if os.path.splitext(file_path)[1] == str_dot_ext:  
+            file_list.append(file_path)
+            #print(file_path)
+        #
+    return file_list;
+    #
+    
+def getTargetTxtFile(img_file):
+    #
+    pre_dir = os.path.abspath(os.path.dirname(img_file)+os.path.sep+"..")
+    txt_dir = os.path.join(pre_dir, 'contents')
+    #
+    filename = os.path.basename(img_file)
+    arr_split = os.path.splitext(filename)
+    filename = arr_split[0] + '.txt'
+    #
+    txt_file = os.path.join(txt_dir, filename)
+    #
+    return txt_file
+    #
+
+def getImageSize(img_file):
+    #
+    img = Image.open(img_file)
+    return img.size  # (width, height)
+    #
+
+def getListContents(content_file):
+    #
+    contents = []
+    #
+    with open(content_file, 'r') as fp:
+        lines = fp.readlines()
+    #
+    for line in lines:
+        arr_str = line.split('|')
+        item = list(map(lambda x: int(x), arr_str[0].split('-')))
+        #
+        contents.append([item, arr_str[1][:-1]])
+        #
+    return contents
+
+#
+# util function
+#
+def load_data(dir_data):
+    #
+    dir_images = dir_data + '/images'
+    #dir_contents = dir_data + '/contents'
+    #
+    list_imgs = getFilesInDirect(dir_images, meta.str_dot_img_ext)
+    #
+    data = []
+    labels = []
+    #
+    for img_file in list_imgs:
+        #
+        # label
+        txt_file = getTargetTxtFile(img_file)
+        content_list = getListContents(txt_file)
+        #
+        list_chars = list(map(meta.mapChar2Order, content_list[0][1])) 
+        #
+        if len(list_chars) != meta.num_chars: continue
+        #
+        #print(img_file)
+        #print(content_list[0][1])
+        #print(list_chars)
+        #
+        labels.append(list_chars)  #
+        #
+        # img_data
+        img = Image.open(img_file)
+        img = np.array(img, dtype = np.float32)/255
+        img = img[:,:,0:3]
+        #
+        data.append(img)  #
+        #
+        '''
+        try:
+            chosen = random.choice(range(len(content_list)))
+            list_chars = list(map(mapChar2Order, content_list[chosen][1]))  
+        except BaseException:
+            print('character OUT OF ALPHABET')
+            print(content_list[chosen][1])
+            chosen = -1
+        '''
+    
+    #
+    return {'x': data, 'y': labels}
+    #
+
---
+++ script_recog.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+@author: limingfan
+
+"""
+
+import model_comm_meta as meta
+
+import model_recog_data as model_data
+from model_recog_wrap import ModelRecog
+
+
+import os
+#
+os.environ['CUDA_VISIBLE_DEVICES'] = '0' #使用 GPU 0
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0,1' # 使用 GPU 0，1
+#
+
+
+
+
+#
+model = ModelRecog()
+#
+
+
+# data
+print('loading data ...')
+data_train = model_data.load_data(meta.dir_data_train)
+data_valid = model_data.load_data(meta.dir_data_valid)
+print('load finished.')
+
+#
+# train
+model.train_and_valid(data_train, data_valid)
+#
+
+
+#
+# predict
+model.load_pb_for_prediction()
+sess = model.create_session_for_prediction()
+#
+list_images_valid = model_data.getFilesInDirect(meta.dir_images_valid, meta.str_dot_img_ext)
+for img_file in list_images_valid:
+    #
+    # img_file = './data_test/images/bkgd_1_0_generated_0.png'
+    #
+    print(img_file)
+    #
+    model.predict(sess, img_file, out_dir = './results_prediction')
+    #
+
